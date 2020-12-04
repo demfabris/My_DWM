c284c6a9d4ca607776f2c7425d455794c0eaab7c
fabricio7p <fabriciodematte7p@gmail.com>
2020-12-04 03:17:35 -0300

    brave hotkey

--- config.def.h
+++ config.def.h
@@ -126,6 +126,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_s,      show,           {0} },
 	{ MODKEY,                       XK_h,      hide,           {0} },
 	{ MODKEY,                       XK_i,      spawn,          SHCMD("firefox") },
+	{ MODKEY,                       XK_l,      spawn,          SHCMD("brave-bin --force-device-scale-factor=1.125") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
